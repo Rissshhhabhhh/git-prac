3d393e93ad112bde7a2ab7981d3308aba8dd262a
Ashish Singh <rishabh.ashish-singh@capgemini.com>
2026-07-16 15:18:48 +0530

    added subtract_num function and updated main file

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-from util.calculator import add_num
+from util.calculator import add_num , subtract_num
 if __name__ == "__main__":
     print (add_num (1,3))
+    print (subtract_num(5,3))
+    
--- util/calculator.py
+++ util/calculator.py
@@ -1,2 +1,4 @@
 def add_num (a,b):
     return a+b
+def subtract_num (a,b):
+    return a-b
