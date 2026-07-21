bf878ebb3fca1e3a79b02564b093458c003b5132
Ashish Singh <rishabh.ashish-singh@capgemini.com>
2026-07-21 13:02:11 +0530

    added labels to print statements

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from util.calculator import add_num , divide_num , subtract_num , power_num
 if __name__ == "__main__":
-    print (add_num (1,3))
-    print (divide_num(5,3))
-    print (subtract_num(5,3))
-    print (power_num(2,4))
+    print ("Adding:", add_num (1,3))
+    print ("Dividing", divide_num(5,3))
+    print ("Subtracting:",subtract_num(5,3))
+    print ("Powering:",power_num(2,4))
     
     
