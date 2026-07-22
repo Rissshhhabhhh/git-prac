dac1981bf3a70e86827a0c97b3f106275b0b1741
Ashish Singh <rishabh.ashish-singh@capgemini.com>
2026-07-22 17:10:21 +0530

    Revert "updated main.py"
    This reverts commit 019be4b3cd2fb3317c2fb02479876c41d6c126dc.

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     print ("Adding:", add_num (1,3))
     print ("Dividing", divide_num(5,3))
     print ("Subtracting:",subtract_num(5,3))
-    print ("Powering:",power_num(3,4))
+    print ("Powering:",power_num(2,4))
     
     
